e80587b63748e4e6288ddd54bb8fb739e166ab37
Yashraj36 <yashrajingale70@gmail.com>
2024-10-18 19:32:42 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! We will get back to you shortly.');
+    // Here you could add code to send the form data to a server
+});
